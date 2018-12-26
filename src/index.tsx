56853e23bd24b48645e5fc6a4c2f82de1e356280
ong-ar <reclusis@gmail.com>
2018-12-26 22:57:01 +0900

    graphql 관련 코드 제거

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,5 @@
 import * as React from "react";
-import { ApolloProvider } from "react-apollo";
 import * as ReactDOM from "react-dom";
-import client from "./apollo";
 import App from "./Routes/App";
 
-ReactDOM.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
-  document.getElementById("root") as HTMLElement
-);
+ReactDOM.render(<App />, document.getElementById("root") as HTMLElement);
