7d655246963f74a2ce26abbb055b3a3a12a2b92a
ong-ar <reclusis@gmail.com>
2019-04-10 17:47:25 +0900

    패키지 정리 및 gh-pages 추가

--- src/Routes/App/AppPresenter.tsx
+++ src/Routes/App/AppPresenter.tsx
@@ -1,13 +1,9 @@
-import "core-js/es6/number";
-import "core-js/es7/array";
-import "es6-shim";
 import * as React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import "url-search-params-polyfill";
 import Home from "../Home";
 
 const AppPresenter: React.SFC = () => (
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <Routes />
   </BrowserRouter>
 );
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,6 @@
+import "react-app-polyfill/ie9";
+import "core-js/es6/number";
+import "core-js/es7/array";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import App from "./Routes/App";
